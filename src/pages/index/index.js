9cb2b5cd78bdce9c1909b873da910d37f883931b
Joachim <jo-brasier@outlook.com>
2019-08-30 16:57:10 +0200

    Render posts on index page

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import PropTypes from 'proptypes';
+import { connect } from 'react-redux';
+import { MDBContainer } from 'mdbreact';
+import { Link } from 'react-router-dom';
+import { getPosts } from '../../redux/actions/postActions';
 
 class Index extends Component {
+  async componentDidMount() {
+    await this.props.getPosts(); // eslint-disable-line
+  }
+
   render() {
-    return <div>Index</div>;
+    const { posts } = this.props.posts; // eslint-disable-line
+
+    return (
+      <MDBContainer className="py-5">
+        {posts.map((post) => (
+          <div key={post._id}>
+            <small className="d-block">{post.createdAt}</small>
+            <Link to={`/${post.slug}`}>{post.title}</Link>
+            <p>{post.content}</p>
+          </div>
+        ))}
+      </MDBContainer>
+    );
   }
 }
 
-export default Index;
+Index.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  posts: PropTypes.shape({
+    posts: PropTypes.arrayOf(PropTypes.object),
+  }).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  posts: state.posts,
+});
+
+export default connect(
+  mapStateToProps,
+  { getPosts },
+)(Index);
