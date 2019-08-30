44159c3e06e50bd4316eaf4e41ae9d9223cdf4b7
Joachim <jo-brasier@outlook.com>
2019-08-30 16:56:01 +0200

    Add redux types

---
+++ src/redux/types.js
@@ -0,0 +1,13 @@
+// Post types
+export const GET_POSTS = 'GET_POSTS';
+export const GET_POST = 'GET_POST';
+export const ADD_POSTS = 'ADD_POSTS';
+export const DELETE_POSTS = 'DELETE_POSTS';
+export const UPDATE_POSTS = 'UPDATE_POSTS';
+
+// Error types
+export const GET_ERRORS = 'GET_ERRORS';
+export const CLEAN_ERRORS = 'CLEAN_ERRORS';
+
+// Loading types
+export const LOADING = 'LOADING';
