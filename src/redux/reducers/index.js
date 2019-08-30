615e30674a1d36d099706d8d334265a55a177ad3
Joachim <jo-brasier@outlook.com>
2019-08-30 16:57:20 +0200

    Add reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
+import errorReducer from './errorReducer';
+import postReducer from './postReducer';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  errors: errorReducer,
+  posts: postReducer,
+});
 
 export default rootReducer;
