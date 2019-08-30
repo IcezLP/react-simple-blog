297c9b983e031193fcfdfc9b2c6aeb71bc2d84b9
Joachim <jo-brasier@outlook.com>
2019-08-30 12:12:50 +0200

    Add mdbootstrap

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages/App';
 
+// CSS import
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
