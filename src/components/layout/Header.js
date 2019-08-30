9c4c24c6c5649db40726d756858a7cdaac4874ec
Joachim <jo-brasier@outlook.com>
2019-08-30 13:39:29 +0200

    Add navbar

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBNavLink,
+} from 'mdbreact';
+import { Link } from 'react-router-dom';
 
-const Header = () => {
-  return <div>Header</div>;
-};
+class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  render() {
+    const { isOpen } = this.state;
+
+    return (
+      <MDBNavbar color="blue" dark expand="md">
+        <MDBNavbarBrand className="white-text">
+          <Link to="/" className="white-text">
+            React Simple Blog
+          </Link>
+        </MDBNavbarBrand>
+        <MDBNavbarToggler onClick={() => this.setState({ isOpen: !isOpen })} />
+        <MDBCollapse isOpen={isOpen} navbar>
+          <MDBNavbarNav right>
+            <MDBNavItem>
+              <MDBNavLink to="/login">Login</MDBNavLink>
+            </MDBNavItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBNavbar>
+    );
+  }
+}
 
 export default Header;
