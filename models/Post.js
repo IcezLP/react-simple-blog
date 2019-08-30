7168ea1ea6db796721af59fe09acb040ff34358f
Joachim <jo-brasier@outlook.com>
2019-08-30 14:45:26 +0200

    add post schema

---
+++ models/Post.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const postSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: Date,
+});
+
+module.exports = Post = model('post', postSchema);
