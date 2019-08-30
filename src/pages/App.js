fe31f5e6781e596183de7a657b581c4cf4b53eda
Joachim <jo-brasier@outlook.com>
2019-08-30 12:08:56 +0200

    Add basic functionnal header/footer components

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+// Layout components import
+import Header from '../components/layout/Header';
+import Footer from '../components/layout/Footer';
+
+// Pages import
 import Index from './index';
 
 const App = () => (
   <Router>
+    <Header />
     <Route exact path="/" component={Index} />
+    <Footer />
   </Router>
 );
 
