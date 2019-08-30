040ec3cf1701799c02c4cf72224447fdc4edf397
Joachim <jo-brasier@outlook.com>
2019-08-30 20:53:24 +0200

    Update post redux

--- src/redux/actions/postActions.js
+++ src/redux/actions/postActions.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
-import { GET_POSTS, GET_ERRORS } from '../types';
+import { GET_POSTS, GET_POST, GET_ERRORS, LOADING } from '../types';
 
 const BASE_URL = '/api/v1/post';
 
 // Get all posts
 export const getPosts = () => async (dispatch) => {
+  await dispatch({
+    type: LOADING,
+  });
+
   try {
     const posts = await axios.get(BASE_URL);
     await dispatch({
@@ -12,15 +16,34 @@ export const getPosts = () => async (dispatch) => {
       payload: posts.data,
     });
   } catch (err) {
+    console.log(err);
     await dispatch({
       type: GET_ERRORS,
-      payload: err.response.data,
+      payload: err,
     });
   }
 };
 
 // Get post by slug
-export const getPost = () => async (dispatch) => {};
+export const getPost = (slug) => async (dispatch) => {
+  await dispatch({
+    type: LOADING,
+  });
+
+  try {
+    const posts = await axios.get(`${BASE_URL}/${slug}`);
+    await dispatch({
+      type: GET_POST,
+      payload: posts.data,
+    });
+  } catch (err) {
+    console.log(err);
+    await dispatch({
+      type: GET_ERRORS,
+      payload: err,
+    });
+  }
+};
 
 // Create new post
 export const createPost = () => async (dispatch) => {};
--- src/redux/reducers/postReducer.js
+++ src/redux/reducers/postReducer.js
@@ -1,16 +1,29 @@
-import { GET_POSTS } from '../types';
+import { GET_POSTS, LOADING, GET_POST } from '../types';
 
 const initialState = {
   posts: [],
   post: {},
+  loading: false,
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
     case GET_POSTS:
       return {
         ...state,
         posts: action.payload,
+        loading: false,
+      };
+    case GET_POST:
+      return {
+        ...state,
+        post: action.payload,
+        loading: false,
       };
     default:
       return state;
