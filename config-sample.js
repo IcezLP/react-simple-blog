97a90329003417038fd69e0c045d3c0052b53124
Joachim <jo-brasier@outlook.com>
2019-09-01 14:37:51 +0200

    update config sample

--- config-sample.js
+++ config-sample.js
@@ -2,4 +2,12 @@ module.exports = {
   PORT: '',
   URL: '',
   DB: '',
+  ADMIN_EMAIL: '',
+  ADMIN_USER: '',
+  ADMIN_PASSWORD: '',
+  SMTP_HOST: '',
+  SMTP_PORT: '',
+  SMTP_USER: '',
+  SMTP_PASSWORD: '',
+  SECRET_TOKEN: '',
 };
